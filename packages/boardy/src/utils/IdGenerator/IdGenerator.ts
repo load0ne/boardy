6469ed218cc99474ee52e7c08ad42c56436b425d
load0ne <jjazan201@gmail.com>
2020-08-30 16:23:33 +0900

    [ BUGFIX ] add polyfill - Math.imul #31

--- packages/boardy/src/utils/IdGenerator/IdGenerator.ts
+++ packages/boardy/src/utils/IdGenerator/IdGenerator.ts
@@ -12,8 +12,17 @@ export default class IdGenerator {
   static hashStringToNumber(str: String): number {
     let h = 0;
     for (let i = 0; i < str.length; i++) {
-      h = Math.imul(31, h) + str.charCodeAt(i) | 0;
+      h = IdGenerator.imul(31, h) + str.charCodeAt(i) | 0;
     }
     return Math.abs(h);
   }
+
+  // mdn polyfill (Math.imul)
+  static imul(a: number, b: number): number {
+    const aHi = (a >>> 16) & 0xffff;
+    const aLo = a & 0xffff;
+    const bHi = (b >>> 16) & 0xffff;
+    const bLo = b & 0xffff;
+    return ((aLo * bLo) + (((aHi * bLo + aLo * bHi) << 16) >>> 0) | 0);
+  };
 }
